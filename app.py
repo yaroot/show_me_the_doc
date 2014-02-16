2324ea9bf687f3385ae3f4d8e51c3a012a92e28d
yaroot <tsu@yaroot.net>
2014-02-16 11:15:55 +0800

    Add support for rst, html and binary blob

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import codecs
 from flask import Flask, request, make_response
 from flask.ext.autoindex import AutoIndex
 import markdown2
+from docutils.core import publish_string
 
 _REPO_DIR = os.path.join(os.environ['HOME'], 'repos')
 
@@ -15,43 +16,74 @@ app = Flask(__name__)
 HOME = os.environ['HOME']
 autoindex = AutoIndex(app, _REPO_DIR, add_url_rules=False)
 
-md_exts = [
-    'md',
-    'mkd',
-    'markdown',
+def render_markdown(content):
+    return markdown2.markdown(content)
+
+def render_rst(content):
+    return publish_string(source=content, writer_name='html4css1')
+
+renderers = [
+    {
+        'ext': ['md', 'mkd', 'markdown'],
+        'render_func': render_markdown,
+    },
+    {
+        'ext': ['rst'],
+        'render_func': render_rst,
+    },
 ]
 
-def is_md_ext(path):
-    path = path.lower()
-    for ext in md_exts:
+def test_exts(path, exts):
+    for ext in exts:
         if path.endswith('.%s' % ext):
             return True
     return False
 
+def get_render_func(path):
+    renderer = None
+    for r in renderers:
+        if test_exts(path.lower(), r['ext']):
+            renderer = r
+            break
+    if renderer is not None:
+        render_func = renderer['render_func']
+        return render_func
+    return None
 
 def read_file(fp):
-    with codecs.open(fp, encoding='utf-8') as f:
-        return f.read()
+    try:
+        with codecs.open(fp, encoding='utf-8') as f:
+            return f.read()
+    except:
+        with open(fp, mode='rb') as f:
+            return f.read()
 
 
 @app.route('/', defaults={'path':'.'})
 @app.route('/<path:path>')
 def show_me_the_doc(path):
     mdfile = path
-    path = os.path.join(_REPO_DIR, path)
-    if os.path.exists(path):
-        if os.path.isdir(path):
+    abspath = os.path.join(_REPO_DIR, mdfile)
+    if os.path.exists(abspath):
+        if os.path.isdir(abspath):
             return autoindex.render_autoindex(path=mdfile, endpoint='.show_me_the_doc')
         else:
+            render_func = get_render_func(abspath)
             raw = 'raw' in request.args
-            if raw or not is_md_ext(path):
-                content = read_file(path)
+            content = read_file(abspath)
+
+            is_html = abspath.lower().endswith('.html')
+            if is_html and not raw:
+                return content
+            elif raw or render_func is None:
                 resp = make_response(content)
-                resp.mimetype = 'text/plain'
+                if type(content) == unicode:
+                    resp.mimetype = 'text/plain'
+                else:
+                    resp.mimetype = ''
                 return resp
             else:
-                content = read_file(path)
-                rendered = markdown2.markdown(content)
+                rendered = render_func(content)
                 return rendered
     else:
         return '<h1>NOTHING TO SEE HERE</h1>', 404
